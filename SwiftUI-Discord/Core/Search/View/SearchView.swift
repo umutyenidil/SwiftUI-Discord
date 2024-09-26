4c6fa2665ea59dd075a696d3a24fabbb1b070388
Umut Yenidil <umut.yenidil9@gmail.com>
2024-09-26 10:23:55 +0300

    SearchView duzenlendi

--- SwiftUI-Discord/Core/Search/View/SearchView.swift
+++ SwiftUI-Discord/Core/Search/View/SearchView.swift
@@ -8,8 +8,34 @@
 import SwiftUI
 
 struct SearchView: View {
+    var users = ["json", "levi", "elon", "timi"]
+    @State var searchText = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            List(users, id: \.self) { user in
+                HStack {
+                    Text(user.prefix(1))
+                        .font(.largeTitle)
+                        .frame(width: 32)
+                        .padding()
+                        .foregroundStyle(.white)
+                        .background {
+                            Circle()
+                                .fill(Color(.discord))
+                        }
+                    
+                    Text(user)
+                        .font(.title)
+                }
+            }
+            .frame(maxHeight: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
+            .background(Color(.background))
+            .scrollContentBackground(.hidden)
+            .searchable(text: $searchText)
+            .navigationTitle("Users")
+        }
+        .preferredColorScheme(/*@START_MENU_TOKEN@*/.dark/*@END_MENU_TOKEN@*/)
     }
 }
 
